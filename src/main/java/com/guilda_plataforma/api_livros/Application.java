84702e29ac5cf097657d0e850e9cbbc93e8677ef
= <=>
2021-03-30 15:52:32 -0300

    update: ajustes

--- src/main/java/com/guilda_plataforma/api_livros/Application.java
+++ src/main/java/com/guilda_plataforma/api_livros/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiLivrosApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiLivrosApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
