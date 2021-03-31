e06a4eff8b8367cca895c239889265ef5f72b0c4
= <=>
2021-03-31 15:19:21 -0300

    update: pesquisa personalizada de tema

--- src/main/java/com/guilda_plataforma/api_livros/repository/LivroRepository.java
+++ src/main/java/com/guilda_plataforma/api_livros/repository/LivroRepository.java
@@ -4,7 +4,10 @@ import com.guilda_plataforma.api_livros.entities.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LivroRepository extends JpaRepository<Livro, String> {
 
+    List<Livro> findAllByTemaIgnoreCase(String tema);
 }
